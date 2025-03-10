716ea93a71a5cd8a11f9add7aaa20010023ecf25
Skylar Gagnon <144175053+Reddest-Panda@users.noreply.github.com>
2025-03-10 11:26:23 -0400

    Delete debug.py

--- debug.py
+++
@@ -1,8 +0,0 @@
-import time
-from modules.parser import Parser
-from modules.classifier import Classifier
-from modules.generator import Generator
-from modules.logger import Logger
-
-classifier = Classifier("arm/pmc_template.c", "configs/measure.xml")
-print(classifier.get_results("test"))
